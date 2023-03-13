7626af07f88485a1a322503475cea1c9a9ba261f
viacaelestis <affectuminflamma@gmail.com>
2023-03-13 11:16:54 -0600

    search bar works, you can drop markers on map now

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -87,38 +87,38 @@ function initAutocomplete() {
     // For each place, get the icon, name and location.
     const bounds = new google.maps.LatLngBounds();
 
-    // places.forEach((place) => {
-    //   if (!place.geometry || !place.geometry.location) {
-    //     console.log("Returned place contains no geometry");
-    //     return;
-    //   }
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
 
-    //   const icon = {
-    //     url: place.icon,
-    //     size: new google.maps.Size(71, 71),
-    //     origin: new google.maps.Point(0, 0),
-    //     anchor: new google.maps.Point(17, 34),
-    //     scaledSize: new google.maps.Size(25, 25),
-    //   };
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
 
       // Create a marker for each place.
-      // markers.push(
-      //   new google.maps.Marker({
-      //     map,
-      //     icon,
-      //     title: place.name,
-      //     position: place.geometry.location,
-      //   })
-      // );
-    //   if (place.geometry.viewport) {
-    //     // Only geocodes have viewport.
-    //     bounds.union(place.geometry.viewport);
-    //   } else {
-    //     bounds.extend(place.geometry.location);
-    //   }
-    // });
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
     map.fitBounds(bounds);
   });
 }
 
-// window.initAutocomplete = initAutocomplete;
+window.initAutocomplete = initAutocomplete;
