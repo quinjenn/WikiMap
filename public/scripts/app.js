d422bc662359825f6eee6521ff00ca912efa808e
viacaelestis <affectuminflamma@gmail.com>
2023-03-13 18:23:38 -0600

    add minor edits to format and make sure baseline app is working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -23,6 +23,8 @@ const locations = [
   { lat: -42.735258, lng: 147.438 },
   { lat: -43.999792, lng: 170.463352 },
 ];
+
+// declare global variable for map
 let map;
 // Initialize and add the map
 function initMap() {
@@ -47,25 +49,18 @@ function initMap() {
 window.initMap = initMap;
 
 
-// code for search bar
-
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+// implementation search bar
 function initAutocomplete() {
 
   // Create the search box and link it to the UI element.
-  const input = document.getElementById("pac-input");
-  const searchBox = new google.maps.places.SearchBox(input);
+const input = document.getElementById("pac-input");
+const searchBox = new google.maps.places.SearchBox(input);
 
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener("bounds_changed", () => {
-    searchBox.setBounds(map.getBounds());
-  });
+map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener("bounds_changed", () => {
+      searchBox.setBounds(map.getBounds());
+    });
 
   let markers = [];
 
@@ -78,14 +73,8 @@ function initAutocomplete() {
       return;
     }
 
-    // Clear out the old markers.
-    // markers.forEach((marker) => {
-    //   marker.setMap(null);
-    // });
-    // markers = [];
-
     // For each place, get the icon, name and location.
-    const bounds = new google.maps.LatLngBounds();
+const bounds = new google.maps.LatLngBounds();
 
     places.forEach((place) => {
       if (!place.geometry || !place.geometry.location) {
--- routes/users-api.js
+++ routes/users-api.js
@@ -8,6 +8,11 @@
 const express = require('express');
 const router  = express.Router();
 const userQueries = require('../db/queries/users');
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: { rejectUnauthorized: false }
+// });
 
 router.get('/', (req, res) => {
   userQueries.getUsers()
@@ -20,5 +25,23 @@ router.get('/', (req, res) => {
         .json({ error: err.message });
     });
 });
+
+// // will add longitude and latitude of every place searched to sql database
+// router.post('/points', async (req, res) => {
+//   const { name, latitude, longitude } = req.body;
+//   const client = await pool.connect();
+//   try {
+//     await client.query('BEGIN');
+//     await client.query('INSERT INTO places (name, latitude, longitude) VALUES ($1, $2, $3)', [name, latitude, longitude]);
+//     await client.query('COMMIT');
+//     res.sendStatus(200);
+//   } catch (error) {
+//     await client.query('ROLLBACK');
+//     console.error(error);
+//     res.sendStatus(500);
+//   } finally {
+//     client.release();
+//   }
+// });
 
 module.exports = router;
