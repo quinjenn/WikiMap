2590557d4b91def87b0fd123024d9c9969f29f67
viacaelestis <affectuminflamma@gmail.com>
2023-03-14 16:29:23 -0600

    create router and query file for markers

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+// routes for Google Maps Markers object
+const markerRoutes = require('./routes/markers-routes');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -38,6 +40,8 @@ const usersRoutes = require('./routes/users');
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
+// any request with url /api/markers will be handled by markerRoutes router
+app.use('api/markers', markerRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
