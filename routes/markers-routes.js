afffef8935cbe8c1f4899416707f671703548382
viacaelestis <affectuminflamma@gmail.com>
2023-03-15 02:09:06 -0600

    commit a version where we know the map still works

--- routes/markers-routes.js
+++ routes/markers-routes.js
@@ -5,7 +5,24 @@ const db = require('../db/connection');
 // import markersData array
 const markersData = require('public/scripts/app.js');
 
+// ** This code works assuming that I have already obtained the mapId value from the maps table somehow
+// loop through each item in the markersData array
+markersData.forEach(async (marker) => {
+  try {
+    // construct SQL query with placeholders for the parameterized values
+    const query = `
+    INSERT INTO points (latitude, longitude, name, map_id)
+    VALUES ($1, $2, $3, $4)
+    `;
 
+    // execute the query with the parameterized values
+    const result = await db.query(query, [marker.lat, marker.lng, marker.name, mapId]);
+
+    console.log(`Inserted ${result.rowCount} row(s)`);
+  } catch (err) {
+    console.error(err);
+  }
+})
 
 
 module.exports = router;
