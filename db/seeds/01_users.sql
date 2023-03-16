f651f260685ee500b49c8ed0f39219dc824f450a
quinjenn <jquintal_@hotmail.com>
2023-03-15 22:50:30 -0700

    JNM db route to browser and fixed bugs

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (username, password, email) VALUES ('Alice', 'asdfasdf', 'asdf@csco.com');
+INSERT INTO users (username, password, email) VALUES ('JNMaps', 'Map Ninja', 'jnmaps@me.com');
 INSERT INTO users (username, password, email) VALUES ('Kira', 'Kirapassword', 'kira@me.com');
 INSERT INTO users (username, password, email) VALUES ('Namra', 'Namraassword', 'namra@me.com');
+INSERT INTO users (username, password, email) VALUES ('Jenn', 'Jennpassword', 'jenn@me.com');
--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,5 +1,6 @@
-INSERT INTO maps (title, description, image_url, user_id)
-VALUES ('Map 1', 'This is map 1', 'https://example.com/map1.jpg', 1),
+INSERT INTO maps (id, title, description, image_url, user_id)
+VALUES (10,'JNMaps', 'Our Favourite Places', ' https://cdn.britannica.com/71/94371-050-293AE931/Mountains-region-Ten-Peaks-Moraine-Lake-Alberta.jpg
+', 1),
 INSERT INTO maps (title, description, image_url, user_id)
 VALUES ('Map 2', 'This is map 2', 'https://example.com/map2.jpg', 2);
 
