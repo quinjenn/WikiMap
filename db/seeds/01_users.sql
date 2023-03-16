072a97a9fd83cde91dde8e1833001db435cc997e
quinjenn <jquintal_@hotmail.com>
2023-03-15 20:11:27 -0700

    pushing branch to git

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
 INSERT INTO users (username, password, email) VALUES ('Alice', 'asdfasdf', 'asdf@csco.com');
 INSERT INTO users (username, password, email) VALUES ('Kira', 'Kirapassword', 'kira@me.com');
+INSERT INTO users (username, password, email) VALUES ('Namra', 'Namraassword', 'namra@me.com');
