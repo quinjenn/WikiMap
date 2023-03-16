48b1280250ca8c4c8f954273630bf7ec8562985b
namraaslam <namra.aslam@ucalgary.ca>
2023-03-16 10:08:24 -0600

    add edit button in app, and edit marker div

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,7 +1,15 @@
+// Favorite toggle button function
 function toggleFocus(button) {
   if (button.classList.contains("focus")) {
     button.classList.remove("focus");
   } else {
     button.classList.add("focus");
   }
+}
+
+
+// Hide Div function
+function togglePointsList() {
+  const pointsList = document.querySelector('.points-list-container');
+  pointsList.style.display = pointsList.style.display === 'none' ? 'block' : 'none';
 }
